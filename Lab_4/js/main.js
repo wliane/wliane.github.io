f2609ee1b572019d16cb2b725ab10811ad6c1393
wliane <71611210+wliane@users.noreply.github.com>
2020-12-16 20:50:28 -0500

    commit

--- Lab_4/js/main.js
+++ Lab_4/js/main.js
@@ -21,19 +21,19 @@ myContent.appendChild(mp2);
 
 // 3 Into the first paragraph, insert the phrase "my name has " length of your name " characters"
 //      (e.g. my name has 10 characters).
-let length = myFullName.length - 1;
+let length = myFullName.length - 2;
 mp1.innerText = "My name has " + length + " characters.";
 
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 const third = myFullName[2].toUpperCase();
-const last3 = myFullName.substring(10,12);
+const last3 = myFullName.substring(11,14);
 mp2.innerText = "The 3rd character in my name is " + third + ". \n" + "The last 3 characters of my last name are " + last3;
 // 6 Add a new line to your second paragraph
 
 
 // 8 Substring your first and last name into two separate variables
 const first = myFullName.substring(0,5);
-const last = myFullName.substring(6,11);
+const last = myFullName.substring(6,13);
 
 // 9 Add the total length of your first and last names together
 const total = first.length + last.length;
